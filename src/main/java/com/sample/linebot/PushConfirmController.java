ffce84326e492a41f3f520519367f6683c8e0523
horiuchit71 <takuya.horiuchi@g.softbank.co.jp>
2018-09-27 01:32:51 +0900

    comment add

---
+++ src/main/java/com/sample/linebot/PushConfirmController.java
@@ -0,0 +1,44 @@
+package com.sample.linebot;
+
+import java.net.URISyntaxException;
+import java.util.concurrent.ExecutionException;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.PushMessage;
+import com.linecorp.bot.model.action.MessageAction;
+import com.linecorp.bot.model.message.TemplateMessage;
+import com.linecorp.bot.model.message.template.ConfirmTemplate;
+import com.linecorp.bot.model.response.BotApiResponse;
+
+@RestController
+public class PushConfirmController {
+
+    private final LineMessagingClient lineMessagingClient;
+
+    PushConfirmController(LineMessagingClient lineMessagingClient) {
+        this.lineMessagingClient = lineMessagingClient;
+    }
+
+    //リマインドをプッシュ
+    @GetMapping("alarm")
+    public void pushAlarm() throws URISyntaxException {
+
+        try {
+            BotApiResponse response = lineMessagingClient
+                                            .pushMessage(new PushMessage("/* プッシュしたい人のuserId */",
+                                                         new TemplateMessage("明日は燃えるごみの日だよ！",
+                                                                 new ConfirmTemplate("ごみ捨ては終わった？",
+                                                                         new MessageAction("はい", "はい"),
+                                                                         new MessageAction("いいえ", "いいえ")
+                                                                 )
+                                                         )))
+                                            .get();
+            System.out.println("MYLOG: Sent messages: {}" + response);
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/com/sample/linebot/ScheduledTaskService.java
+++ src/main/java/com/sample/linebot/ScheduledTaskService.java
@@ -6,6 +6,9 @@ import java.text.SimpleDateFormat;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.client.LineMessagingClientImpl;
+
 //@Slf4j
 @Service
 public class ScheduledTaskService {
@@ -15,12 +18,29 @@ public class ScheduledTaskService {
     @Scheduled(cron="${cron.cron3}", zone = "Asia/Tokyo")
     public void executeAlarm() throws URISyntaxException {
         //プッシュする処理を呼び出す
-		pushAlarm();
+    	LineMessagingClient client = new LineMessagingClientImpl(null);
+    	PushConfirmController controller = new PushConfirmController(client);
+    	controller.pushAlarm();
     }
 
-	private void pushAlarm() {
-		// TODO Auto-generated method stub
-		System.out.println("schedule execute");
-		
-	}
+//	private void pushAlarm() {
+//		// TODO Auto-generated method stub
+//		System.out.println("MYLOG: schedule execute");
+//	    try {
+//	        Object lineMessagingClient;
+//			BotApiResponse response = lineMessagingClient
+//	                                        .pushMessage(new PushMessage(properties.getTargetId(),
+//	                                                     new TemplateMessage("明日は燃えるごみの日だよ！",
+//	                                                             new ConfirmTemplate("ごみ捨ては終わった？",
+//	                                                                     new MessageAction("はい", "はい"),
+//	                                                                     new MessageAction("いいえ", "いいえ")
+//	                                                             )
+//	                                                     )))
+//	                                        .get();
+//	        System.out.println("MYLOG: Sent messages: " + response);
+//	    } catch (InterruptedException | ExecutionException e) {
+//	    	System.out.println("MYLOG: RuntimeException is occured");
+//	        throw new RuntimeException(e);
+//	    }
+//	}
 }
